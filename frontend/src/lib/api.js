93e3efd74d4c85c21626bdb23f86968ff75812f0
조형민 <jhm1994gd@naver.com>
2025-09-25 20:26:56 +0900

    api url chaged

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -14,7 +14,7 @@ const fastapi = (operation, url, params, success_callback, failure_callback) => 
         body = qs.stringify(params)
     }
 
-    let _url = 'http://127.0.0.1:8000'+url
+    let _url = import.meta.env.VITE_SERVER_URL+url
     if(method === 'get') {
         _url += "?" + new URLSearchParams(params)
     }
